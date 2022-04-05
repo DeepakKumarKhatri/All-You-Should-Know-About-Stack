8d45d800a27d5e035ea9e2aed1a22360a646b33a
Deepak Kumar <86526696+DeepakKumarKhatri@users.noreply.github.com>
2022-04-05 22:35:11 +0500

    Add files via upload

---
+++ parenthesisMatching.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+struct DStack{
+    char data;
+    DStack* next = NULL;
+};
+DStack* top = NULL;
+
+void push(char i);
+char pop();
+bool isEmpty();
+bool parenthesisMatching();
+
+int main(){
+    cout<<"Is Parenthesis Matching: "<<parenthesisMatching;
+}
+bool parenthesisMatching(){
+    char *exp;
+    exp = "((a+b)*(c-d))";
+    for(int i=0;i<exp[i]!='\0';i++){
+        if(exp[i]=='('){
+            push(exp[i]);
+        }
+        else if(exp[i]==')'){
+            if(isEmpty()){
+                return false;
+            } else {
+                pop();
+            }
+        }
+    }
+    if(isEmpty()){
+        return true;
+    } else{
+        return false;
+    }
+}
+void push(char element){
+    DStack *newNode = new DStack;
+    cin>>newNode->data;
+    if(newNode==NULL){
+        cout<<"Stack Is Full (STACK-OVER-FLOW)";
+    }
+    if(top==NULL){
+        top = newNode;
+    }else{
+        newNode->next = top;
+        top = newNode;
+    }
+}
+char pop(){
+    char x = -1;
+    DStack *p = top;
+    if(top==NULL){
+        cout<<"Stack Is Empty (STACK-UNDER-FLOW)";
+    }else{
+        x = top->data;
+        top = top->next;
+        delete p;
+    }
+    return x;
+
+}
+bool isEmpty() {
+    if (top == NULL) {
+        return true;
+    } else {
+        return false;
+    }
+}
